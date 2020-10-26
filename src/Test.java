76d68293dd4a9e07553ec23f67bb5a07a226190b
ldblovepeace <853126656@qq.com>
2020-10-26 14:06:40 +0800

    finish Q622

--- src/Test.java
+++ src/Test.java
@@ -1,15 +1,32 @@
 import Questions.Q1;
+import Questions.Q622;
 
 public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Q1 q1 = new Q1();
-		int[] i = new int[] {1,2,7,14};
-		int j = 9;
-		int[] result = q1.twoSum(i, j);
-		System.out.println(result[0]);
-		System.out.println(result[1]);
+//		Q1
+		/*
+		 * Q1 q1 = new Q1(); int[] i = new int[] {1,2,7,14}; int j = 9; int[] result =
+		 * q1.twoSum(i, j); System.out.println(result[0]);
+		 * System.out.println(result[1]);
+		 */
+//		Q622
+//		["MyCircularQueue","enQueue","Front","isFull","enQueue","enQueue","enQueue","deQueue","enQueue","enQueue","isEmpty","Rear"]
+//				[[4],[3],[],[],[7],[2],[5],[],[4],[2],[],[]]
+		Q622 c = new Q622(4);
+		
+		System.out.println(c.enQueue(3));
+		System.out.println(c.Front());
+		System.out.println(c.isFull());
+		System.out.println(c.enQueue(7));
+		System.out.println(c.enQueue(2));
+		System.out.println(c.enQueue(5));
+		System.out.println(c.deQueue());
+		System.out.println(c.enQueue(4));
+		System.out.println(c.enQueue(2));
+		System.out.println(c.isEmpty());
+		System.out.println(c.Rear());
 	}
 
 }
