1ee54ba2f9c41921f3cc5ec10d824e6c1a43141d
ldblovepeace <853126656@qq.com>
2020-10-26 18:00:06 +0800

    完成Q1365的两种解法，排序方法未实现

---
+++ src/Questions/Q1365.java
@@ -0,0 +1,40 @@
+package Questions;
+
+public class Q1365 {
+//	暴力解法，直接从头到尾遍历
+	public int[] smallerNumbersThanCurrent1(int[] nums) {
+		int r = 0;
+		int[] result = new int[nums.length];
+		for(int i=0; i< nums.length; i++) {
+			for(int j=0; j < nums.length; j++) {
+				if(nums[i] >nums[j]) {
+					r=r+1;
+				}
+			}
+			result[i]=r;
+			r=0;
+		}
+		return result;
+    }
+//	排序
+//	计数
+	public int[] smallerNumbersThanCurrent3(int[] nums) {
+		int count[] = new int[101];
+		int result[] = new int[nums.length];
+		for(int i=0; i< nums.length; i++) {
+			count[nums[i]] = count[nums[i]]+1;
+			}
+		for(int i=0; i< nums.length; i++) {
+			result[i] = lessCount(count,nums[i]);
+		}
+		return result;
+		}
+	
+	public int lessCount(int[] nums, int i) {
+		int r=0;
+		for(int j=0;j<i+1;j++) {
+			r=r+nums[j];
+		}
+		return r;
+	}
+}
