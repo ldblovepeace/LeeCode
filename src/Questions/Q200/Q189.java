19358e38951a7efb270a0c1591817d101cc57bfb
ldblovepeace <853126656@qq.com>
2020-11-24 14:08:20 +0800

    Q189的第二种方法
    reverse数组

--- src/Questions/Q200/Q189.java
+++ src/Questions/Q200/Q189.java
@@ -1,7 +1,7 @@
 package Questions.Q200;
 
 public class Q189 {
-    public void rotate(int[] nums, int k) {
+    public void rotate1(int[] nums, int k) {
         int n = nums.length;
         k = k%n;
         
@@ -15,4 +15,61 @@ public class Q189 {
             nums[0] = tmp;
         }
     }
+    
+    public void rotate2(int[] nums, int k) {
+        int n = nums.length-1;
+        k = k % n;
+        
+        if(k == 0) {
+            return;
+        }
+        
+        reverse(nums,0,n);
+        reverse(nums,0,k-1);
+        reverse(nums,k,n);
+    }
+    
+    public void reverse(int[] nums, int i, int j) {
+        int tmp;
+        if(i> nums.length-1 || j > nums.length-1 || i>=j) {
+            return;
+        }
+        while(i<j) {
+            tmp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = tmp;
+            i++;
+            j--;
+        }
+    }
+    
+//    public void rotate3(int[] nums, int k) {
+//        int n=nums.length - 1;
+//        k = k%n;
+//        rotate(nums,k,0,n);
+//    }
+//    
+//    public void rotate(int[] nums, int k, int start, int end) {
+//        int n = end - start - 1;
+//        if(n == 0) {
+//            return;
+//        }
+//        if(n == 1) {
+//            swap(nums, start, end);
+//         }
+//        
+//        for(int i = start; i < k; i++) {
+//            swap(nums, i , i+n-k);
+//        }
+//        
+//        rotate(nums,k,n-k,n);
+//        
+//        k = k%n;
+//    }
+//    
+//    public void swap(int[] nums, int i, int j) {
+//        int tmp = nums[i];
+//        nums[i] = nums[j];
+//        nums[j] = tmp;
+//    }
 }
